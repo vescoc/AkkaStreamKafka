08ddea24c643089022a2c1abf254595cc7ccd052
vescoc <claudio.vesco@gmail.com>
2018-05-14 22:15:08 +0200

    commitSync initial implementation

--- src/main/scala/sample/ConsumerSource.scala
+++ src/main/scala/sample/ConsumerSource.scala
@@ -5,7 +5,8 @@ import org.slf4j.LoggerFactory
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 
-import org.apache.kafka.clients.consumer.{Consumer, ConsumerRecord}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.clients.consumer.{Consumer, ConsumerRecord, OffsetAndMetadata}
 
 import akka.NotUsed
 import akka.stream.{SourceShape, Outlet, Attributes}
@@ -26,17 +27,38 @@ class ConsumerSource[K, V](_consumer: => Consumer[K, V], topics: Seq[String], ti
 
     consumer.subscribe(topics.asJavaCollection)
 
-    override protected def onTimer(timerKey: Any) {
-      log.debug("onTimer {}", timerKey)
-      doPoll()
+    private var committedTopicPartitions = Map.empty[(String, Int), Long]
+
+    private def getCommit(consumerRecord: ConsumerRecord[K, V]): Option[CommitInfo] = {
+      def make = {
+        committedTopicPartitions = committedTopicPartitions + ((consumerRecord.topic, consumerRecord.partition) -> consumerRecord.offset)
+
+        Some(CommitInfo(
+          new TopicPartition(consumerRecord.topic, consumerRecord.partition),
+          new OffsetAndMetadata(consumerRecord.offset)
+        ))
+      }
+
+      committedTopicPartitions.get((consumerRecord.topic, consumerRecord.partition)) match {
+        case Some(l) if l < consumerRecord.offset =>
+          make
+        case Some(_) =>
+          None
+        case None =>
+          make
+      }
     }
 
     private def nextHandler(iterator: Iterator[ConsumerRecord[K, V]]) = new OutHandler {
       def doPush =
         if (iterator.hasNext) {
-          val consumerRecord = iterator.next
+          val _consumerRecord = iterator.next
 
-          val record = Record(consumerRecord)
+          val record = new Record[K, V] {
+            val consumerRecord = _consumerRecord
+
+            def commitSync = getCommit(consumerRecord) foreach { x => consumer.commitSync(Map(x.topic -> x.offset).asJava) }
+          }
 
           log.debug("nextHandler push {}", record)
           push(out, record)
@@ -62,6 +84,11 @@ class ConsumerSource[K, V](_consumer: => Consumer[K, V], topics: Seq[String], ti
       setHandler(out, nextHandler(iterator))
     }
 
+    override protected def onTimer(timerKey: Any) {
+      log.debug("onTimer {}", timerKey)
+      doPoll()
+    }
+
     setHandler(out, new OutHandler {
       def onPull {
         log.debug("initHandler onPull")
@@ -73,7 +100,16 @@ class ConsumerSource[K, V](_consumer: => Consumer[K, V], topics: Seq[String], ti
 object ConsumerSource {
   def apply[K, V](consumer: => Consumer[K, V], topics: Seq[String], timeout: FiniteDuration = 100.millis, sleepTime: FiniteDuration = 1.second): Source[Record[K, V], NotUsed] = Source.fromGraph(new ConsumerSource(consumer, topics, timeout, sleepTime))
 
-  case class Record[K, V](consumerRecord: ConsumerRecord[K, V]) {
+  trait Record[K, V] {
+    val consumerRecord: ConsumerRecord[K, V]
+
+    def key = consumerRecord.key
     def value = consumerRecord.value
+
+    def commitSync: Unit
+
+    override def toString = s"Record[${consumerRecord}]"
   }
+
+  private [ConsumerSource] case class CommitInfo(topic: TopicPartition, offset: OffsetAndMetadata)
 }
--- src/test/scala/sample/ConsumerSourceSpec.scala
+++ src/test/scala/sample/ConsumerSourceSpec.scala
@@ -11,6 +11,7 @@ import akka.testkit.TestKit
 import akka.stream.testkit.scaladsl.TestSink
 
 import org.scalatest.{MustMatchers, WordSpecLike, BeforeAndAfterAll}
+import org.scalatest.concurrent.ScalaFutures
 
 import org.slf4j.LoggerFactory
 
@@ -19,6 +20,7 @@ class ConsumerSourceSpec
     with WordSpecLike
     with MustMatchers
     with BeforeAndAfterAll
+    with ScalaFutures
 {
   val topic = "test"
   val partition = 0
@@ -58,12 +60,13 @@ class ConsumerSourceSpec
   "consumer source" must {
     "handle records one a time" in {
       withResource(new MockConsumer[String, String](OffsetResetStrategy.EARLIEST)) { consumer =>
+        val tp = new TopicPartition(topic, partition)
+
         val sourceUnderTest = ConsumerSource(consumer, Seq(topic))
 
         consumer.schedulePollTask(() => {
           val record1 = TestConsumerRecord[String, String](topic, partition, 0, null, "ciccio")
           val record2 = TestConsumerRecord[String, String](topic, partition, 1, null, "cunicio")
-          val tp = new TopicPartition(topic, 0)
 
           consumer.rebalance(Seq(tp).asJava)
           consumer.updateBeginningOffsets(Map[TopicPartition, java.lang.Long](tp -> 0L).asJava)
@@ -72,21 +75,22 @@ class ConsumerSourceSpec
         })
 
         sourceUnderTest
-          .map(_.value)
-          .runWith(TestSink.probe[String])
+          .map { x => (x.key, x.value) }
+          .runWith(TestSink.probe[(String, String)])
           .request(1)
-          .expectNext("ciccio")
+          .expectNext((null, "ciccio"))
           .request(1)
-          .expectNext("cunicio")
+          .expectNext((null, "cunicio"))
       }
     }
 
     "handle records two a time" in {
       withResource(new MockConsumer[String, String](OffsetResetStrategy.EARLIEST)) { consumer =>
+        val tp = new TopicPartition(topic, partition)
+
         val sourceUnderTest = ConsumerSource(consumer, Seq(topic))
 
         consumer.schedulePollTask(() => {
-          val tp = new TopicPartition(topic, 0)
           val record1 = TestConsumerRecord[String, String](topic, partition, 0, null, "ciccio")
           val record2 = TestConsumerRecord[String, String](topic, partition, 1, null, "cunicio")
 
@@ -106,10 +110,11 @@ class ConsumerSourceSpec
 
     "handle records multiple" in {
       withResource(new MockConsumer[String, String](OffsetResetStrategy.EARLIEST)) { consumer =>
+        val tp = new TopicPartition(topic, partition)
+
         val sourceUnderTest = ConsumerSource(consumer, Seq(topic))
 
         consumer.schedulePollTask(() => {
-          val tp = new TopicPartition(topic, 0)
           val record = TestConsumerRecord[String, String](topic, partition, 0, null, "ciccio")
 
           consumer.rebalance(Seq(tp).asJava)
@@ -132,6 +137,67 @@ class ConsumerSourceSpec
         t
           .request(1)
           .expectNext("cunicio")
+      }
+    }
+
+    "sync commit a record" in {
+      withResource(new MockConsumer[String, String](OffsetResetStrategy.EARLIEST)) { consumer =>
+        val tp = new TopicPartition(topic, partition)
+
+        val sourceUnderTest = ConsumerSource(consumer, Seq(topic))
+
+        consumer.schedulePollTask(() => {
+          val record1 = TestConsumerRecord[String, String](topic, partition, 0, null, "ciccio")
+
+          consumer.rebalance(Seq(tp).asJava)
+          consumer.updateBeginningOffsets(Map[TopicPartition, java.lang.Long](tp -> 0L).asJava)
+          consumer.addRecord(record1)
+        })
+
+        sourceUnderTest
+          .map { r =>
+            r.commitSync
+            r
+          }
+          .map(_.value)
+          .runWith(TestSink.probe[String])
+          .request(1)
+          .expectNext("ciccio")
+
+        consumer.committed(tp).offset must be(0)
+      }
+    }
+
+    "sync commit two records, bad order" in {
+      withResource(new MockConsumer[String, String](OffsetResetStrategy.EARLIEST)) { consumer =>
+        val tp = new TopicPartition(topic, partition)
+
+        val sourceUnderTest = ConsumerSource(consumer, Seq(topic))
+
+        consumer.schedulePollTask(() => {
+          val record1 = TestConsumerRecord[String, String](topic, partition, 0, null, "ciccio")
+          val record2 = TestConsumerRecord[String, String](topic, partition, 1, null, "cunicio")
+
+          consumer.rebalance(Seq(tp).asJava)
+          consumer.updateBeginningOffsets(Map[TopicPartition, java.lang.Long](tp -> 0L).asJava)
+          consumer.addRecord(record1)
+          consumer.addRecord(record2)
+        })
+
+        sourceUnderTest
+          .grouped(2)
+          .map { l =>
+            println(s"l=$l")
+            l(1).commitSync
+            l(0).commitSync // nop
+            l
+          }
+          .runWith(TestSink.probe[Seq[ConsumerSource.Record[String, String]]])
+          .request(1)
+          .expectNext
+
+        val c = consumer.committed(tp)
+        c.offset must be(1)
       }
     }
   }
